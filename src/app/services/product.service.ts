47d76da74ed6ef1e18daf971672c6e59e653e231
amynee <tlijaniamine9@gmail.com>
2024-02-08 14:18:24 +0100

    product details

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,7 +9,6 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root'
 })
 export class ProductService {
-  
     private baseUrl = 'http://localhost:8080/api/products';
     private categoryUrl = 'http://localhost:8080/api/product-category';
 
@@ -22,11 +21,11 @@ export class ProductService {
         return this.getProducts(searchUrl)
     }
 
-  private getProducts(searchUrl: string): Observable<Product[]> {
-    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
-      map(response => response._embedded.products)
-    );
-  }
+    private getProducts(searchUrl: string): Observable<Product[]> {
+        return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(
+        map(response => response._embedded.products)
+        );
+    }
 
     getProductCategories():  Observable<ProductCategory[]> {
         return this.httpClient.get<GetResponseProductCategories>(this.categoryUrl).pipe(
@@ -39,6 +38,13 @@ export class ProductService {
         const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
           
         return this.getProducts(searchUrl)
+    }
+
+    getProduct(theProductId: number): Observable<Product> {
+        // need tp build URL based on the product id
+        const productUrl = `${this.baseUrl}/${theProductId}`;
+
+        return this.httpClient.get<Product>(productUrl);
     }
 }
 
